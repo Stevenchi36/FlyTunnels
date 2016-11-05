c5dac0310f0476c33c6d5161c1f692befe5e49a0
Steven Childrey <steven.childrey36@yahoo.com>
2016-11-04 23:08:22 -0400

    Initial Add

---
+++ android/src/com/stevenchi/flytunnels/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.stevenchi.flytunnels;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.stevenchi.flytunnels.FlyTunnels;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlyTunnels(), config);
+	}
+}
---
+++ desktop/src/com/stevenchi/flytunnels/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stevenchi.flytunnels.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stevenchi.flytunnels.FlyTunnels;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlyTunnels(), config);
+	}
+}
