1d498aa9d1fb642e6201d7396430d3e9351f00b4
Steven Childrey <steven.childrey36@yahoo.com>
2016-11-04 23:50:54 -0400

    GameStateManager Working

--- core/src/com/stevenchi/flytunnels/FlyTunnels.java
+++ core/src/com/stevenchi/flytunnels/FlyTunnels.java
@@ -5,28 +5,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.stevenchi.flytunnels.States.GameStateManager;
+import com.stevenchi.flytunnels.States.MenuState;
 
 public class FlyTunnels extends ApplicationAdapter {
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 480;
 	public static final String TITLE = "Fly Tunnels";
 
-	SpriteBatch batch;
+	private GameStateManager gsn;
+	private SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsn = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsn.push(new MenuState(gsn));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsn.update(Gdx.graphics.getDeltaTime());
+		gsn.render(batch);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
 	}
 	
 	@Override
---
+++ core/src/com/stevenchi/flytunnels/States/MenuState.java
@@ -0,0 +1,29 @@
+package com.stevenchi.flytunnels.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Steven on 11/4/2016.
+ */
+
+public class MenuState extends States {
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
