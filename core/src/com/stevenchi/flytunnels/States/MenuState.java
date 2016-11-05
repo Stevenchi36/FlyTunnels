843a068e880d32c73417de3a357635c904547430
Steven Childrey <steven.childrey36@yahoo.com>
2016-11-05 00:17:34 -0400

    Added background and play button images

--- core/src/com/stevenchi/flytunnels/States/MenuState.java
+++ core/src/com/stevenchi/flytunnels/States/MenuState.java
@@ -1,6 +1,8 @@
 package com.stevenchi.flytunnels.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.stevenchi.flytunnels.FlyTunnels;
 
 /**
  * Created by Steven on 11/4/2016.
@@ -8,8 +10,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MenuState extends States {
 
+    private Texture background;
+    private Texture playButton;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("background.png");
+        playButton = new Texture("playButton.png");
     }
 
     @Override
@@ -24,6 +31,9 @@ public class MenuState extends States {
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, FlyTunnels.WIDTH, FlyTunnels.HEIGHT);
+        sb.draw(playButton, (FlyTunnels.WIDTH / 2) - (playButton.getWidth() / 2), (FlyTunnels.HEIGHT / 2) - playButton.getHeight() / 2);
+        sb.end();
     }
 }
